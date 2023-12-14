9efa4360f12c8b923129fd9f7626132557a661ba
vibhuti2023 <129948528+vibhuti2023@users.noreply.github.com>
2023-12-14 22:09:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,6 @@ let todolist = [];
     });*/
 // Export app
 module.exports = app;
-*//*!
 * Start Bootstrap - Freelancer v7.0.7 (https://startbootstrap.com/theme/freelancer)
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
